8d95663e76e78a85fc1dd884ac1a94d12adc657b
Hasan Bilgin <hasanbilgintr@gmail.com>
2023-12-18 17:56:04 +0300

    for loop added

--- MyTestProjeGithub/ViewController.swift
+++ MyTestProjeGithub/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myArray = [10,20,30,40,50]
+        
+        for num in myArray {
+            print(num)
+        }
+        //1.commit atıyoruz
     }
 
 
